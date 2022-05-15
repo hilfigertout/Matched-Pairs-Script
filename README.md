8ec78995712a0b1bfe49fd9c7011d0c3ba633f8a
hilfigertout <ijr2399@gmail.com>
2022-05-15 11:54:48 -0400

    Added files and documentation

---
+++ README.md
@@ -0,0 +1,67 @@
+# Matched-Pairs voting script
+
+This program implements a system for a single user to rank items using a matched-pairs approach. 
+
+The program takes in a list of options as strings (either from an input text file or entered manually), shuffles the list, and presents the user a pair of options. The user selects which option they prefer. That item gets 1 point. The program continues for every possible pairing of the options, then it presents the full list of options ranked by score, with the first item having the most points and the last item having the least points. (Scores are also shown.)
+
+## Running the program
+
+This program is implemented in Python. To run it, you will need the Python interpreter installed. 
+
+To run this program, simply open a termminal, navigate to the directory containing the matched-pairs.py script, and run the following:
+
+> python3 matched-pairs.py
+
+This script does not take any command line arguments.
+
+## Example Output:
+
+> Enter name of input text file. (Blank line to enter candidates manually.)
+> example_input_file.txt
+> Answer each prompt with either "1" or "2".
+> Enter which of the two you prefer:
+>     1: Option B
+>     2: Option C
+> 1
+> Enter which of the two you prefer: 
+>     1: Option B
+>     2: Option A
+> 2
+> Enter which of the two you prefer: 
+>     1: Option B
+>     2: Option E
+> 1
+> Enter which of the two you prefer: 
+>     1: Option B
+>     2: Option D
+> 1
+> Enter which of the two you prefer: 
+>     1: Option C
+>     2: Option A
+> 2
+> Enter which of the two you prefer: 
+>     1: Option C
+>     2: Option E
+> 1
+> Enter which of the two you prefer: 
+>     1: Option C
+>     2: Option D
+> 1
+> Enter which of the two you prefer: 
+>     1: Option A
+>     2: Option E
+> 1
+> Enter which of the two you prefer: 
+>     1: Option A
+>     2: Option D
+> 1
+> Enter which of the two you prefer: 
+>     1: Option E
+>     2: Option D
+> 2
+> RANKED RESULTS (Option: Score)
+> Option A: 4
+> Option B: 3
+> Option C: 2
+> Option D: 1
+> Option E: 0
---
+++ matched-pairs.py
@@ -0,0 +1,73 @@
+import random
+#This program implements a pairwise comparison for ranking.
+
+def get_candidates():
+    cand_list = []
+    print("Enter the name of the first candidate: ")
+    cand_name = input()
+    while (cand_name != ""):
+        cand_list.append(cand_name)
+        print("Enter the name of the next candidate. (blank line to stop): ")
+        cand_name = input()
+    return cand_list
+
+def read_candidates_from_file(filename):
+    cand_list = []
+    with open(filename) as in_file:
+        cand_list = in_file.readlines()
+    cand_list = [ line.strip() for line in cand_list ]
+    return cand_list
+
+def get_score_order(cand_dict):
+    winner_list = []
+    for i in range(len(cand_dict)):
+        winning_name = ""
+        max = -1
+        for x in cand_dict:
+            if cand_dict[x] > max:
+                winning_name = x
+                max = cand_dict[x]
+        winner_list.append(winning_name)
+        cand_dict.pop(winning_name)
+    return winner_list
+
+def main():
+    print("Enter name of input text file. (Blank line to enter candidates manually.)")
+    filename = input()
+    cand_list = []
+    if (filename == ""):
+        cand_list = get_candidates()
+    else:
+        cand_list = read_candidates_from_file(filename)
+    random.shuffle(cand_list)
+    score_list = [0]*len(cand_list)
+    print("Answer each prompt with either \"1\" or \"2\".")
+    for i in range(len(cand_list)):
+        for j in range(i + 1, len(cand_list)):
+            print("Enter which of the two you prefer: ")
+            print("    1: " + cand_list[i])
+            print("    2: " + cand_list[j])
+            choice = input()
+            try: 
+                if (int(choice) == 1):
+                    score_list[i] = score_list[i] + 1
+                elif (int(choice) == 2):
+                    score_list[j] = score_list[j] + 1
+                else:
+                    print("That's not a valid answer.")
+                    j = j - 1
+            except:
+                print("That's not a valid answer")
+                j = j - 1
+
+    cand_dict = {}
+    for i in range(len(cand_list)):
+        cand_dict[cand_list[i]] =  score_list[i]
+    cand_dict_clone = cand_dict.copy()
+    winner_list = get_score_order(cand_dict_clone)
+    print("RANKED RESULTS (Option: Score)")
+    for x in winner_list:
+        print(x + ": " + str(cand_dict[x]))
+
+if __name__ == "__main__":
+    main()
